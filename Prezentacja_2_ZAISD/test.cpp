1c00df63263003e4732b1231fa36a2977629bc0c
Adrixop95 <adrixop95@me.com>
2018-05-07 14:33:18 +0200

    New code

--- Prezentacja_2_ZAISD/test.cpp
+++ Prezentacja_2_ZAISD/test.cpp
@@ -1,41 +1,58 @@
 #include <iostream>
-#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-int ciag[100]; 
+int ciag[100];
+
+bool checkDuplicates(int array[], int n)
+{
+	sort(array, array + n);
+	int i;
+	for (i = 0; i < n - 1; i++)
+	{
+		if (array[i] == array[i + 1])
+			return true;
+	}
+	return false;
+}
 
 void dalej(int poz, int pozostalo)
 {
+	if (pozostalo == 0) {
+		if (checkDuplicates(ciag, poz) == false){
+			for (int i = 1; i <= poz - 1; i++) {
+				cout << ciag[i] << " ";
+			}
+			cout << endl;
+		}
+	}
 
-    
-    if(pozostalo == 0 && ciag[poz-1] != ciag[poz-2]) {
-        for(int i = 1; i <= poz-1; i++) {
-            cout << ciag[i] << " ";
-        }
-        cout << endl;
-    }
-    else {
-        for(int k = ciag[poz-1]; k <= pozostalo; k++) {
-            ciag[poz] = k;
-            dalej(poz+1, pozostalo - k);
-        }
-    }
+	else {
+		for (int k = ciag[poz - 1]; k <= pozostalo; k++) {
+			ciag[poz] = k;
+			dalej(poz + 1, pozostalo - k);
+		}
+	}
 }
 
 void podzialy(int n)
 {
-    ciag[0] = 1;    
-    dalej(1, n);  
+	ciag[0] = 1;
+	dalej(1, n);
 }
 
-int main(){
+int main() {
 
-    int liczba = 0;
+	int liczba = 0;
 
-    cin >> liczba;
-    podzialy(liczba);
+	cout << "Podaj liczbe: " << endl;
+	cin >> liczba;
+	cin.get();
+	cout << "======" << endl;
 
-    cin.get();
-    return 0;
+	podzialy(liczba);
+
+	cin.get();
+	return 0;
 }
